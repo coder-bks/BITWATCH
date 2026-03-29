3a88be8dc8551037141d452bcd7624aac01e9dd3
balkrishna sawant <balkrishnaraghunathsawant@gmail.com>
2026-03-30 00:01:36 +0530

    Bot ready for deployment

--- bot.py
+++ bot.py
@@ -4,14 +4,45 @@ from config import TOKEN
 from scraper import scrapping
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is running with polling.")
+    await update.message.reply_text("Heyy, Welcome. How can I help You? type /services to view all the services.")
 async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
     fresh_data = scrapping()
     await update.message.reply_text(f'{fresh_data}')
 
+async def send_alert(context: ContextTypes.DEFAULT_TYPE):
+    fresh_data = scrapping()
+    await context.bot.send_message(chat_id=context.job.chat_id, text=fresh_data)
+
+async def alert(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.job_queue.run_repeating(
+        send_alert, 
+        interval=3600, 
+        chat_id=update.effective_chat.id, 
+        name="price_alert"
+    )
+    await update.message.reply_text("Alert service started! You'll get updates every hour.")
+
+async def stop_alert(update, context):
+    jobs = context.job_queue.get_jobs_by_name("price_alert")
+    for job in jobs:
+        job.schedule_removal()
+    await update.message.reply_text("Alert service stopped!")
+
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+   await update.message.reply_text("/start - Greetings /check - to check current bitcoin price /alert - to start automatic price view service /stop_alert - to stop automatic price view service")
+
+
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("check",check))
+app.add_handler(CommandHandler("alert",alert))
+app.add_handler(CommandHandler("stop_alert",stop_alert))
+app.add_handler(CommandHandler("help_command",services))
+
 
 # IMPORTED IN THREADING
 # app.run_polling()
+
+#add package python-telegram-bot[job-queue]
--- scheduler.py
+++ scheduler.py
@@ -4,16 +4,28 @@ from checker import checking
 import schedule
 import time
 
-def job():
-    fresh_data = scrapping()          
-    has_changed = checking(fresh_data)
-    if has_changed:
-        notify(f"Alert! Quote changed: {fresh_data}")  
 
-schedule.every(10).seconds.do(job)
+def job():
+    try:
+        fresh_data = scrapping()
+        has_changed = checking(fresh_data)
+        if has_changed:
+            notify(f"Alert! Bitcoin price changed: {fresh_data}")
+    except Exception as e:
+        print(f"Job failed: {e}. Will retry next run.")
+
+schedule.every(1).hours.do(job)
 
 
 # IMPORTED IN THREADING
 # while True:
 #     schedule.run_pending()
 #     time.sleep(60)
+
+# TURNED to try and except AS API MAY FALL SOMETIMES
+# def job():
+#     fresh_data = scrapping()          
+#     has_changed = checking(fresh_data)
+#     if has_changed:
+#         notify(f"Alert! Quote changed: {fresh_data}")  
+ 
